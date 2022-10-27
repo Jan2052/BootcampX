009641552d132b1f6deb55ed223378a4f0f32c5a
Janet <Janet_Lam@live.ca>
2022-10-26 21:48:36 -0400

    stored potentiall malicious values in an array

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
+let argv1 = process.argv[2];
+let values = [`${argv1}%`];
 
 pool.query(`
 SELECT DISTINCT t.name as teacher, c.name as cohort
@@ -15,9 +16,9 @@ FROM teachers t
 JOIN assistance_requests ON teacher_id = t.id
 JOIN students s ON student_id = s.id
 JOIN cohorts c ON cohort_id = c.id
-WHERE c.name = '${process.argv[2] || 'JUL02'}'
+WHERE c.name = '${argv1 || 'JUL02'}'
 ORDER BY teacher;
-`)
+`, values)
 .then(res=> {
   res.rows.forEach(row=> {
     console.log(`${row.cohort}: ${row.teacher}`)
